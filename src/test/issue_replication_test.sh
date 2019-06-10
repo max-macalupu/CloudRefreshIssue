89d7bb587f0c18e0f16bf9ca72ec72278cbd81c9
Max Macalupu <mmacalupu@creditsesame.com>
2019-06-10 11:46:36 -0500

    changes in the script

--- src/test/issue_replication_test.sh
+++ src/test/issue_replication_test.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-curl -X GET \
-  http://localhost:9000/greeting \
-  -H 'Postman-Token: 53bf9f76-fe0c-4579-af07-c77eee6f8c03' \
-  -H 'cache-control: no-cache';
 
-curl -X POST \
-  http://localhost:9000/actuator/refresh \
-  -H 'Postman-Token: 470c0767-1a31-4f08-89b0-383f9f6bf48b' \
-  -H 'cache-control: no-cache';
+curl -X GET http://localhost:9000/greeting;
 
-curl -X GET \
-  http://localhost:9000/greeting \
-  -H 'Postman-Token: 53bf9f76-fe0c-4579-af07-c77eee6f8c03' \
-  -H 'cache-control: no-cache';
+curl -X POST http://localhost:9000/actuator/refresh;
+
+curl -X GET  http://localhost:9000/greeting;
